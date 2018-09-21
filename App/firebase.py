3399a5588dac8e544ec669ee5a777b14cd19d7e1
nurdtechie98 <nurdtechie98@gmail.com>
2018-09-22 05:25:09 +0530

    add queries

--- App/firebase.py
+++ App/firebase.py
@@ -10,13 +10,12 @@ def refresh(user):
 
 db=firebase.database()
 
-lis=[]
 #a={"testing":[1,2,3,4,5,6]}
 #lis.append(a)
 announce=db.child("announce").get(user['idToken']).val() #retrieve a last of values
 donor=db.child("donor").get(user['idToken']).val()
 print(donor)
-donor["id"]=["r/d","f/s/w/m/c/h",["lat","long"],"number","address"]
+donor["3"]=[0,"010000",[19.081812,72.888420],"anto chris","Need water urgently","9327770111","f27 3/4 Sagar Chs Airoli"]
 db.child("donor").set(donor,user['idToken'])
 #announce["shivam"]=["shivam","pawase",10]
 #db.child("announce").set(announce,user['idToken'])
