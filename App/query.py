0dff0436c2919f413a203a7717b20ac0c16b04e7
inishchith <inishchith@gmail.com>
2018-09-22 04:03:24 +0530

    Add minial run

--- App/query.py
+++ App/query.py
@@ -3,55 +3,74 @@ import urllib.parse
 import codecs
 import json,re
 from config import *
+import apiai
+import retreiveData as retreive
 
+client=apiai.ApiAI(APIAIKey)
 
-ccess_token = "" # Enter your Access Token here
-client=apiai.ApiAI(Access_token)
+need_list = ["food","shelter","water","medicine","cloth","hygene"]
 
 def apiai_query(message):
     req=client.text_request()
     req.lang="de"
     req.session_id="<SESSION ID, UNIQUE FOR EACH USER>"
     req.query=message
+
     response=json.loads(req.getresponse().read().decode('utf-8'))
     responseStatus = response['status']['code']
-    if responseStatus==200 :
+
+    print(response)
+
+    if responseStatus == 200 :
         text = response['result']['fulfillment']['speech']
     else:
         text="No Match Found"
+
     result=response["result"]
     param=result["parameters"]
     if len(param.keys())==0:
         return [text,0]
     if "number" in param.keys():
-        pin=param["number"]
-    if "tof" in param.keys():
-        tof=param["tof"]
+        pin_code = param["number"]
+    if "department" in param.keys():
+        department = param["department"][0]
     if "type" in param.keys():
-        typ=param["type"]
-        if typ=="routes":
-            message=message.split("\n")
-            if(len(message)>3):
-                mod=message[3]
+        typ = param["type"][0]
+        if typ == "route":
+            message = message.split("\n")
+            if ( len(message)>3 ) :
+                mode = message[3] 
             else:
-                mod='transit'
-            return [message[1],message[2],mod,2]
-        elif typ=="detail":
-            message=message.split('\n')[1:]
+                mode = 'transit'
+            print([message[1],message[2],mode,2])
+            return [message[1],message[2],mode,2]
+        elif typ == "complete":
+            message = message.split('\n')[1:]
             print(("*"*30)+"  DETAILED LOCATION  " + "*"*30)
             message = ",".join(message)
             print("RECEIVED : ",message)
             return [message,3]
         elif typ=="nearby":
-            return [pin,tof,typ,1]
+            return [pin_code,department,typ,1]
+        elif typ=="emergency":
+            message = message.split('\n')[1:]
+            message = ",".join(message)
+            return [department,message,4]
+        elif typ=="requirement":
+            # department = param["department1"] :  Single : ! LIST 
+            x = ["0"]*6
+            x[need_list.index(department)] = "1"
+            x = "".join(x)
+            message = message.split('\n')
+            description = message[1]
+            message = message[1:]
+            message = ",".join(message)
+            actual_addr = retreive.process_detail(message)
+            lat_long = retreive.latlong(actual_addr)
 
-def getMessages(inboxID):
-    data =  urllib.parse.urlencode({'apikey': TLapikey, 'inbox_id' : inboxID})
-    data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.textlocal.in/get_messages/?")
-    f = urllib.request.urlopen(request, data)
-    fr = f.read()
-    return(fr)
+            return [0,x,lat_long,"N/A",description,actual_addr,5]
+            
+    print("REACHED END")
 
 def sendSMS(number, message):
     data =  urllib.parse.urlencode({'apikey': TLapikey, 'numbers': number,
@@ -62,14 +81,11 @@ def sendSMS(number, message):
     fr = f.read()
     print("MESSAGE SENT")
 
-def response():
-    resp = getMessages('10')
-    ans = json.loads(resp.decode('ASCII'))
-    message = ans["messages"][-1]["message"][6:]
-    sender = ans["messages"][-1]["number"]
-    return ans
-    #return message,sender
-
 if __name__ == "__main__":
     # sendSMS("7045185177","HEY")
-    print(response())
+#     print(apiai_query('''complete address 
+# sheffield apartments
+# dahisar'''))
+    print(apiai_query('''emergency fire at 
+sheffield apartments
+dahisar'''))
